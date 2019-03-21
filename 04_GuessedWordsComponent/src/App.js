445a5fd904e131e28cc946dfd659108fad7686b3
Andrew Menning <menning23@gmail.com>
2019-03-21 18:11:41 -0400

    Lecture 46 Bootstrap styling

--- 04_GuessedWordsComponent/src/App.js
+++ 04_GuessedWordsComponent/src/App.js
@@ -7,7 +7,7 @@ import Congrats from './Congrats';
 class App extends Component {
     render() {
         return (
-            <div className="App" data-test="component-app">
+            <div className="container" data-test="component-app">
                 <h1>Jotto</h1>
                 <Congrats success={true} />
                 <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
