2be49ecf84bb0dbeece587dc3bb3078a8372bbbf
Andrew Menning <menning23@gmail.com>
2019-03-21 19:46:58 -0400

    Lecture 55 Set up connected component tests

---
+++ 05_SimpleReduxSuccessPieceOfState/src/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Input extends Component {
+    render () {
+        return <div></div>
+    }
+}
+
+const mapStoreToProps = (state) => {
+    return {};
+}
+
+export default connect(mapStoreToProps)(Input);
