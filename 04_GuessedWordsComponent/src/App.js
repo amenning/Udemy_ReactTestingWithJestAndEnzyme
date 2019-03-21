b7ff64422916a7ca5ac268d296fa533ff53e22d4
Andrew Menning <menning23@gmail.com>
2019-03-21 18:07:52 -0400

    Lecture 45 Manual Acceptance Testing

--- 04_GuessedWordsComponent/src/App.js
+++ 04_GuessedWordsComponent/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
+
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            counter: 0
-        }
-    }
-
     render() {
         return (
-            <div data-test="component-app">
-                <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
-                <button
-                    data-test="increment-button"
-                    onClick={() => this.setState({counter: this.state.counter + 1})}
-                >
-                    Increment counter
-                </button>
+            <div className="App" data-test="component-app">
+                <h1>Jotto</h1>
+                <Congrats success={true} />
+                <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
             </div>
         );
     }
