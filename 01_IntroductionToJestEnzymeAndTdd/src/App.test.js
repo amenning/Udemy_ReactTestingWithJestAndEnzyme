3cfd5513d17b6b6d320c144f944a083f6c0c103e
Andrew Menning <menning23@gmail.com>
2019-03-20 20:00:30 -0400

    Lecture 10 Using Enzyme in a Test:wq

--- 01_IntroductionToJestEnzymeAndTdd/src/App.test.js
+++ 01_IntroductionToJestEnzymeAndTdd/src/App.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter() });
 
 test('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    // You can print out the DOM using
+    // console.log(wrapper.debug());
 
+    expect(wrapper).toBeTruthy();
 });
