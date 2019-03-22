0587d1fba2ca5c3ab18d49bcc7725895b07bb87a
Andrew Menning <menning23@gmail.com>
2019-03-21 22:57:54 -0400

    Lecture 62 Input Tests and Code for success = true

--- 05_SimpleReduxSuccessPieceOfState/src/Input.test.js
+++ 05_SimpleReduxSuccessPieceOfState/src/Input.test.js
@@ -12,8 +12,7 @@ import { Input } from './Input';
  * @returns {ShallowWrapper}
  */
 const setup = (initialState = {}) => {
-    const store = storeFactory(initialState);
-    const wrapper = shallow(<Input />, { store });
+    const wrapper = shallow(<Input {...initialState} />);
     return wrapper;
 };
 
@@ -42,16 +41,25 @@ describe('render', () =>{
     });
 
     describe('word has been guessed', () => {
-        test('renders component without error', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: true };
+            wrapper = setup(initialState);
+        });
 
+        test('renders component without error', () => {
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1);
         });
 
         test('does not render input box', () => {
-
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(0);
         });
 
         test('does not render submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(0);
         });
     });
 });
