8aa62e00f2890c4ad8d3760f9166aae6d1ff2048
Andrew Menning <menning23@gmail.com>
2019-03-25 22:13:33 -0400

    Lecture 78 Set up `secretWord` Action creator and tests

---
+++ 07_TestingAxios/src/actions/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+import { getLetterMatchCount } from '../helpers';
+
+export const actionTypes = {
+    CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD'
+};
+
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *     and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returs {function} - Redux Thunk function
+ */
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+        const secretWord = getState().secretWord;
+        const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+        dispatch({
+            type: actionTypes.GUESS_WORD,
+            payload: { guessedWord, letterMatchCount }
+        });
+
+        if (guessedWord === secretWord) {
+            dispatch({ type: actionTypes.CORRECT_GUESS });
+        }
+    };
+};
+
+export const getSecretWord = () => {
+    return (dispatch) => {
+        axios.get('http://localhost:3030');
+    }
+}
