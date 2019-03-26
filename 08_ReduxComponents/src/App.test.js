752522bf7c1d0ee6aff76573fd09e92d938135fe
Andrew Menning <menning23@gmail.com>
2019-03-26 17:35:20 -0400

    Lecture 88 `getSecretWord` call on App Mount

--- 08_ReduxComponents/src/App.test.js
+++ 08_ReduxComponents/src/App.test.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { storeFactory } from '../test/testUtils';
-import App from './App';
+import App, { UnconnectedApp } from './App';
 import { getSecretWord } from './actions';
 
 /**
  * @function setup
  * @param {object} state - State for this setup.
+ * @param {function} getSecretWord
  * @returns {ShallowWrapper}
  */
-const setup = (state = {}) => {
-    const store = storeFactory(state);
-    const wrapper = shallow(<App {...state} getSecretWord={getSecretWord} />);
+const setup = (state = {}, getSecretWordMock) => {
+    const wrapper = shallow(<App {...state} getSecretWord={getSecretWordMock || getSecretWord} />);
+    console.log(wrapper.debug());
     return wrapper;
 }
 
@@ -45,4 +46,25 @@ describe('redux properties', () => {
         const getSecretWordProp = wrapper.instance().props.getSecretWord;
         expect(getSecretWordProp).toBeInstanceOf(Function);
     });
+});
+
+test('`getSecretWord` runs on App mount', () => {
+    const getSecretWordMock = jest.fn();
+    const props = {
+        getSecretWord: getSecretWordMock,
+        success: false,
+        guessedWords: []
+    }
+
+    // set up app component with getSecretWordMock as the getSecretWord prop
+    const wrapper = shallow(
+        <UnconnectedApp {...props} />
+    );
+
+    // run lifecycle method
+    wrapper.instance().componentDidMount();
+
+    // check to see if mock ran
+    const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+    expect(getSecretWordCallCount).toBe(1);
 });
